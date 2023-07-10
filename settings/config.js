ddf3d6f699780dcfbf104b9bb9050c4876fae996
Fire Bird <73753258+kabirsingh2004@users.noreply.github.com>
2023-07-10 09:13:15 +0530

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,14 +1,12 @@
 import { Colors } from "discord.js";
 
 const settings = {
-  TOKEN:
-    process.env.TOKEN ||
-    "MTA1NTA3ODUwMTgzNDgyOTg4NA.GeWMAq._RGcvdi4nGki_iJhrcu7U4wbn8J9tVcs8ZRftA",
+  TOKEN: process.env.TOKEN || "Bot_Token",
   PREFIX: "!",
   Owners: ["OwnersId", "OwnersId"],
   Slash: {
     Global: false,
-    GuildID: process.env.GuildID || "903532162236694539",
+    GuildID: process.env.GuildID || "GuildId",
   },
   embed: {
     color: "#F4C430",
